f032b53ba26c37620d962cd6b8687ee2be55b217
toh ker wei <mrtohkerwei@gmail.com>
2020-02-02 00:16:03 +0800

    Revert "no message"
    This reverts commit af70cff2be6f243d238213eca481535cde216d99.

--- Duke.java
+++ Duke.java
@@ -1,32 +1,17 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.List;
 
 public class Duke {
 
     public static Scanner sc = new Scanner(System.in);
 
-
     public static void main(String[] args) {
-
-        ArrayList<String> listsOfInput = new ArrayList<>();
-
         //greetings
         System.out.println("Harlo Sir, how may i help you?");
 
         //echo
         String input = sc.next();
-        while(!input.equals("bye")){
-            if (input.equals("list")){
-                int i = 1;
-                for (String inputs : listsOfInput){
-                    System.out.println(i + ". " + inputs);
-                    i++;
-                }
-            } else {
-                listsOfInput.add(input);
-                System.out.println("added: " + input);
-            }
+        while(!input.equals("bye")) {
+            System.out.println(input);
             input = sc.next();
         }
 

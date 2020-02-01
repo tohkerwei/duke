67409664059da9a95641f4b0fd94699999e08ff0
toh ker wei <mrtohkerwei@gmail.com>
2020-02-02 00:39:45 +0800

    Level 2. Add, List

--- Duke.java
+++ Duke.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
 
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        ArrayList<String> inputs = new ArrayList<>();
+
         //greetings
         System.out.println("Harlo Sir, how may i help you?");
 
         //echo
         String input = sc.next();
-        while(!input.equals("bye")) {
-            System.out.println(input);
+        while (!input.equals("bye")) {
+            if (input.equals("list")){
+                int i = 1;
+                for (String ip : inputs) {
+                    System.out.println(i + ". " + ip);
+                    i++;
+                }
+            } else {
+                inputs.add(input);
+                System.out.println("added: " + input);
+            }
             input = sc.next();
         }
 

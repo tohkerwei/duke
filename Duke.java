10ae7efb99c4251f103547c74d115c33ce65743b
toh ker wei <mrtohkerwei@gmail.com>
2020-02-02 09:13:58 +0800

    Level 3. Mark as Done

--- Duke.java
+++ Duke.java
@@ -7,25 +7,33 @@ public class Duke {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ArrayList<String> inputs = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         //greetings
         System.out.println("Harlo Sir, how may i help you?");
 
         //echo
-        String input = sc.next();
-        while (!input.equals("bye")) {
-            if (input.equals("list")){
+        Task input = new Task(sc.nextLine());
+
+        while (!input.description.equals("bye")) {
+            if (input.description.equals("list")){
                 int i = 1;
-                for (String ip : inputs) {
-                    System.out.println(i + ". " + ip);
+                for (Task task : tasks) {
+                    System.out.println(i + ". " + task);
                     i++;
                 }
+            } else if(input.description.contains("done")){
+                String[] strArray = input.description.split(" ");
+                int index = Integer.parseInt(strArray[1]) -1;
+                Task task = tasks.get(index);
+                task.isDone = true;
+                System.out.println("Good job on getting this done!");
+                System.out.println(task);
             } else {
-                inputs.add(input);
-                System.out.println("added: " + input);
+                tasks.add(input);
+                System.out.println("added: " + input.description);
             }
-            input = sc.next();
+            input = new Task(sc.nextLine());
         }
 
         //bye
---
+++ Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    public String description;
+    public boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString(){
+        String result = "[" + getStatusIcon() + "] " + description;
+        return result;
+    }
+}
